8d2f63704b8f3c36c50a0e6bf10840020a49ed3c
Bartek O <bartek@fos.nl>
2022-04-29 15:15:45 +0200

    Added settings for setting the tft ports, brightnessm sd port and debug mode in Settings.h

--- include/settings.h
+++ include/settings.h
@@ -1,6 +1,13 @@
 #ifndef __SETTINGS_H__
 #define __SETTINGS_H__
 
+#define DEBUG 1
 
+#define TFT_CS 10
+#define TFT_DC 9
+#define TFT_BL 5
+#define SD_CS  4
+
+#define TFT_BRIGHTNESS  127
 
 #endif
