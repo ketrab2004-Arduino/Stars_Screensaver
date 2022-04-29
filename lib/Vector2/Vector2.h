7b79bbeb2ada401c46bcf6ca88cde221245dcf19
Bartek O <bartek@fos.nl>
2022-04-29 15:37:22 +0200

    Created a header file for the Vector2 struct
    with some Vector2 operator overloading

---
+++ lib/Vector2/Vector2.h
@@ -0,0 +1,21 @@
+#ifndef __VECTOR2_H__
+#define __VECTOR2_H__
+
+#include <stdint.h>
+
+struct Vector2
+{
+    int16_t x;
+    int16_t y;
+
+    Vector2();
+    Vector2(int16_t x, int16_t y);
+    Vector2(int16_t xy);
+
+    Vector2 operator+(Vector2& other);
+    Vector2 operator-(Vector2& other);
+    Vector2 operator*(Vector2& other);
+    Vector2 operator/(Vector2& other);
+};
+
+#endif
