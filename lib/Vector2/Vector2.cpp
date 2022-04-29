a151cb8730612b78f11a8afeb664861b8f553cc6
Bartek O <bartek@fos.nl>
2022-04-29 16:09:48 +0200

    Onelined all constructors and operator overloads for Vector2
    readability slightly suffers, but they aren't complicated

--- lib/Vector2/Vector2.cpp
+++ lib/Vector2/Vector2.cpp
@@ -1,109 +1,45 @@
 #include "Vector2.h"
 
 #pragma region Constructors
-Vector2::Vector2()
-{
-    x = 0, y = 0;
-};
-Vector2::Vector2(int16_t x, int16_t y)
-{
-    this->x = x;
-    this->y = y;
-};
-Vector2::Vector2(int16_t xy)
-{
-    x = xy, y = xy;
-};
+Vector2::Vector2() { x = 0, y = 0; }
+Vector2::Vector2(int16_t x, int16_t y) { this->x = x, this->y = y; }
+Vector2::Vector2(int16_t xy) { x = xy, y = xy; }
 #pragma endregion
 
 #pragma region Operators
 // Comparison
-bool Vector2::operator==(Vector2 &other)
-{
-    return x == other.x && y == other.y;
-}
-bool Vector2::operator!=(Vector2 &other)
-{
-    return x != other.x || y != other.y;
-}
+bool Vector2::operator==(Vector2 &other) { return x == other.x && y == other.y; }
+bool Vector2::operator!=(Vector2 &other) { return x != other.x || y != other.y; }
 // Increment/Decrement
-void Vector2::operator++()
-{
-    x++, y++;
-}
-void Vector2::operator--()
-{
-    x--, y--;
-}
+void Vector2::operator++() { x++, y++; }
+void Vector2::operator--() { x--, y--; }
 
 #pragma region arithmetic (Vector2)
-Vector2 Vector2::operator+(Vector2 &other)
-{
-    return Vector2(x + other.x, y + other.y);
-}
-void Vector2::operator+=(Vector2 &other)
-{
-    x += other.x, y += other.y;
-}
-Vector2 Vector2::operator-(Vector2 &other)
-{
-    return Vector2(x - other.x, y - other.y);
-}
-void Vector2::operator-=(Vector2 &other)
-{
-    x -= other.x, y -= other.y;
-}
-Vector2 Vector2::operator*(Vector2 &other)
-{
-    return Vector2(x * other.x, y * other.y);
-}
-void Vector2::operator*=(Vector2 &other)
-{
-    x *= other.x, y *= other.y;
-}
-Vector2 Vector2::operator/(Vector2 &other)
-{
-    return Vector2(x / other.x, y / other.y);
-}
-void Vector2::operator/=(Vector2 &other)
-{
-    x /= other.x, y /= other.y;
-}
+Vector2 Vector2::operator+(Vector2 &other) { return Vector2(x + other.x, y + other.y); }
+void Vector2::operator+=(Vector2 &other) { x += other.x, y += other.y; }
+
+Vector2 Vector2::operator-(Vector2 &other) { return Vector2(x - other.x, y - other.y); }
+void Vector2::operator-=(Vector2 &other) { x -= other.x, y -= other.y; }
+
+Vector2 Vector2::operator*(Vector2 &other) { return Vector2(x * other.x, y * other.y); }
+void Vector2::operator*=(Vector2 &other) { x *= other.x, y *= other.y; }
+
+Vector2 Vector2::operator/(Vector2 &other) { return Vector2(x / other.x, y / other.y); }
+void Vector2::operator/=(Vector2 &other) { x /= other.x, y /= other.y; }
 #pragma endregion
 
 #pragma region arithmetic (int16_t)
-Vector2 Vector2::operator+(int16_t other)
-{
-    return Vector2(x + other, y + other);
-}
-void Vector2::operator+=(int16_t other)
-{
-    x += other, y += other;
-}
-Vector2 Vector2::operator-(int16_t other)
-{
-    return Vector2(x - other, y - other);
-}
-void Vector2::operator-=(int16_t other)
-{
-    x -= other, y -= other;
-}
-Vector2 Vector2::operator*(int16_t other)
-{
-    return Vector2(x * other, y * other);
-}
-void Vector2::operator*=(int16_t other)
-{
-    x *= other, y *= other;
-}
-Vector2 Vector2::operator/(int16_t other)
-{
-    return Vector2(x / other, y / other);
-}
-void Vector2::operator/=(int16_t other)
-{
-    x /= other, y /= other;
-}
+Vector2 Vector2::operator+(int16_t other) { return Vector2(x + other, y + other); }
+void Vector2::operator+=(int16_t other) { x += other, y += other; }
+
+Vector2 Vector2::operator-(int16_t other) { return Vector2(x - other, y - other); }
+void Vector2::operator-=(int16_t other) { x -= other, y -= other; }
+
+Vector2 Vector2::operator*(int16_t other) { return Vector2(x * other, y * other); }
+void Vector2::operator*=(int16_t other) { x *= other, y *= other; }
+
+Vector2 Vector2::operator/(int16_t other) { return Vector2(x / other, y / other); }
+void Vector2::operator/=(int16_t other) { x /= other, y /= other; }
 #pragma endregion
 
 #pragma endregion
