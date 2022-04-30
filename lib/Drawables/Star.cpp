6a080660e330d0333c9df5525373094862990006
Bartek O <bartek@fos.nl>
2022-04-30 18:01:52 +0200

    Added content to the inherited `draw()` and `undraw()` methods of Star

--- lib/Drawables/Star.cpp
+++ lib/Drawables/Star.cpp
@@ -18,9 +18,11 @@ Star::Star(int16_t x, int16_t y, float dir): Drawable(x, y) // extend the drawab
 
 void Star::draw(Adafruit_ILI9341 &tft)
 {
+    tft.writePixel(pos.x, pos.y, ILI9341_WHITE); // draw white star
 }
 void Star::undraw(Adafruit_ILI9341 &tft)
 {
+    tft.writePixel(pos.x, pos.y, ILI9341_BLACK); // fill star with black (background colour)
 }
 void Star::doStep(unsigned long delta, Adafruit_ILI9341 &tft)
 {
